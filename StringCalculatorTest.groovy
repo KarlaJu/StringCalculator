af64b5663b7f8efce96376363c5040053589cd3b
karlaJu <karla@makingdevs.com>
2018-06-08 13:35:08 -0500

    Setup two numbers test

--- StringCalculatorTest.groovy
+++ StringCalculatorTest.groovy
@@ -15,4 +15,14 @@ class StringCalculatorTest extends GroovyTestCase {
     result = stringCalculator.add("7")
     assert result == 7
   }
+
+  void testTwoNumbers(){
+    StringCalculator stringCalculator = new StringCalculator()
+    int result = stringCalculator.add("1,2")
+    assert result == 3
+    result = stringCalculator.add("3,4")
+    assert result == 7
+    result = stringCalculator.add("7,8")
+    assert result == 15
+  }
 }
