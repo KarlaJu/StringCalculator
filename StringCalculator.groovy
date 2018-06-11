340e98d7459f9a573eed42ad358984c702068fc5
karlaJu <karla@makingdevs.com>
2018-06-11 16:01:11 -0500

    Refactoring code

--- StringCalculator.groovy
+++ StringCalculator.groovy
@@ -1,8 +1,18 @@
 class StringCalculator {
-	int add(String getnumbers){
-		if(!getnumbers) 0
-		else{
-      getnumbers.size()? getnumbers.replaceAll("\n",",").split(",")*.toInteger().sum() : getnumbers.toInteger()
-    }
+	int add(String numbers){
+		if(verifyTheStringIsEmpty(numbers)) 0
+		else doTheStringCalculator(numbers)
+  }
+
+  private verifyTheStringIsEmpty(String numbers){
+    numbers=="" || numbers == null
+  }
+
+  private String stringWithoutBreakLines(String numbers){
+    numbers.replaceAll("\n",",")
+  }
+
+  private Integer doTheStringCalculator(String numbers){
+    stringWithoutBreakLines(numbers).split(",")*.toInteger().sum()
   }
 }
