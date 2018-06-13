9d3008506a447c1da20dd22247516f8d7964baf2
karlaJu <karla@makingdevs.com>
2018-06-13 14:34:45 -0500

    Refactoring code

--- StringCalculator.groovy
+++ StringCalculator.groovy
@@ -1,27 +1,36 @@
 class StringCalculator {
 	int add(String numbers){
 		if(verifyTheStringIsEmpty(numbers)) 0
-		else doTheStringCalculator(numbers)
+		else {
+      if(verifyIfTheStringHasNegativeNumbers(numbers)) 0
+      else doTheStringCalculator(numbers)
+    }
   }
 
   private verifyTheStringIsEmpty(String numbers){
     numbers=="" || numbers == null
   }
 
-  private String stringWithoutBreakLines(String numbers){
-    numbers.replaceAll("\n",",")
+  private verifyIfTheStringHasNegativeNumbers(String numbers){
+    if(numbers.contains("-"))
+    {
+      def negativeNumbers=""
+      def numberList=numbers.split(",")*.toInteger()
+      numberList.each{i->  i<0 ? negativeNumbers=negativeNumbers+" "+i : 0}
+      throw new RuntimeException("negatives not allowed"+negativeNumbers)
+    }
   }
   
   private String replaceOtherDelimiterForCommas(String numbers){
-  	String delimiter="\\"+numbers.charAt(2).toString()
+  	String delimiter="\\"+numbers[2].toString()
 		numbers.substring(4).replaceAll(delimiter,",")
   }
 
-  private String verifyIfTheStringHasOtherDelimither(String numbers){
-    numbers.contains("/") ? replaceOtherDelimiterForCommas(numbers) : stringWithoutBreakLines(numbers)
+  private String identifyDelimither(String numbers){
+    numbers.contains("/") ? replaceOtherDelimiterForCommas(numbers) : numbers.replaceAll("\n",",")
   }
   
   private Integer doTheStringCalculator(String numbers){
-    verifyIfTheStringHasOtherDelimither(numbers).split(",")*.toInteger().sum()
+    identifyDelimither(numbers).split(",")*.toInteger().sum()
   }
 }
